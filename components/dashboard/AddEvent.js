9a6fa494eed11a75c5a41754c3c42c95884e21f9
Joseph Micla <josephmicla@gmail.com>
2022-02-19 16:29:54 -0800

    date defaults to today

--- components/dashboard/AddEvent.js
+++ components/dashboard/AddEvent.js
@@ -9,11 +9,21 @@ import DialogTitle from '@mui/material/DialogTitle';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-import { Input } from '@mui/material';
+
+// Output:
+// Today's date formatted as YYYY-MM-DD
+const getTodayString = () => {
+  const today = new Date().toLocaleDateString('en-US').split('/');
+  const month = today[0] < 10 ? `0${today[0]}` : today[0];
+  const day = today[1] < 10 ? `0${today[1]}` : today[1];
+  return [today[2], month, day].join('-');
+};
 
 export default function AddEvent(props) {
   const { openAddEvent, setOpenAddEvent } = props;
   const [type, setType] = useState('Eat');
+  const [date, setDate] = useState(getTodayString());
+  const [notes, setNotes] = useState('');
 
   const handleChange = (event) => {
     setType(event.target.value);
@@ -23,50 +33,75 @@ export default function AddEvent(props) {
     setOpenAddEvent(false);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log({
+      date,
+      type,
+      notes,
+    });
+  };
+
   return (
     <div>
       <Dialog open={openAddEvent} onClose={handleClose}>
-        <DialogTitle>ADD EVENT</DialogTitle>
-        <DialogContent>
-          <DialogContentText>Make a note about your snake.</DialogContentText>
-          <TextField autoFocus margin="dense" id="date" type="date" fullWidth variant="standard" />
-          <InputLabel id="type">Type</InputLabel>
-          <Select labelId="type" id="select-type" value={type} onChange={handleChange}>
-            <MenuItem value={'Eat'}>Eat</MenuItem>
-            <MenuItem value={'Shed'}>Shed</MenuItem>
-            <MenuItem value={'Weight'}>Weight</MenuItem>
-            <MenuItem value={'Poop'}>Poop</MenuItem>
-          </Select>
-          {type !== 'Weight' && (
+        <form onSubmit={handleSubmit}>
+          <DialogTitle>ADD EVENT</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Make a note about your snake.</DialogContentText>
             <TextField
               autoFocus
               margin="dense"
-              id="notes"
-              label="Notes"
-              type="text"
-              multiline
-              minRows={5}
+              id="date"
+              type="date"
               fullWidth
               variant="standard"
+              value={date}
+              onChange={(event) => setDate(event.target.value)}
             />
-          )}
-          {type === 'Weight' && (
-            <TextField
-              autoFocus
-              margin="dense"
-              id="notes"
-              label="Weight in grams"
-              type="number"
-              placeholder="ex. 200"
-              fullWidth
-              variant="standard"
-            />
-          )}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
-        </DialogActions>
+            <InputLabel id="type">Type</InputLabel>
+            <Select labelId="type" id="select-type" value={type} onChange={handleChange}>
+              <MenuItem value={'Eat'}>Eat</MenuItem>
+              <MenuItem value={'Shed'}>Shed</MenuItem>
+              <MenuItem value={'Weight'}>Weight</MenuItem>
+              <MenuItem value={'Poop'}>Poop</MenuItem>
+            </Select>
+            {type !== 'Weight' && (
+              <TextField
+                autoFocus
+                margin="dense"
+                id="notes"
+                label="Notes"
+                type="text"
+                multiline
+                minRows={5}
+                fullWidth
+                variant="standard"
+                value={notes}
+                onChange={(event) => setNotes(event.target.value)}
+              />
+            )}
+            {type === 'Weight' && (
+              <TextField
+                autoFocus
+                margin="dense"
+                id="notes"
+                label="Weight in grams"
+                type="number"
+                placeholder="ex. 200"
+                fullWidth
+                variant="standard"
+                value={notes}
+                onChange={(event) => setNotes(event.target.value)}
+              />
+            )}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit">Subscribe</Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
