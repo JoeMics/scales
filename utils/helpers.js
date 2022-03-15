15e1817f715e097df7b227c17cac55c68b877404
Joseph Micla <josephmicla@gmail.com>
2022-03-14 17:33:55 -0700

    adds doc to formatDate helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,12 @@
+/**
+ * Converts a date object using the browser's locale
+ * @param {Date} date - Date object
+ * @returns {string} formatted date string
+ *
+ * @example
+ * // returns 3/1/2022
+ * formatDate(new Date('March 1,2022'))
+ */
 export function formatDate(date) {
   return new Intl.DateTimeFormat([]).format(date);
 }
